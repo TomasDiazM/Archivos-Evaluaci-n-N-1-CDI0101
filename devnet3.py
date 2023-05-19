c209f6d1af7128bb821412cbd92b815a577ce83e
TomasDiazM <127259471+TomasDiazM@users.noreply.github.com>
2023-05-19 14:39:00 +0000

    Delete devnet3.py

--- devnet3.py
+++
@@ -1,7 +0,0 @@
-acl = print(“Ingrese numero de ACL”)
-if acl >= 1 && acl <= 99:
-    print (“Su ACL es Estándar. ”)
-if acl >= 100 && acl <= 199:
-    print(“Su ACL es Extendida”)
-else:
-print(“Su número no corresponde a una ACL”)
